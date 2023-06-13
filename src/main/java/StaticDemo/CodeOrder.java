022d96ef37b6be263c44094f1b14d43b0c51167a
boma086 <boma086@gmail.com>
2023-06-13 13:14:54 +0800

    commit

--- src/main/java/StaticDemo/CodeOrder.java
+++ src/main/java/StaticDemo/CodeOrder.java
@@ -7,6 +7,8 @@ public class CodeOrder{
 	public static void main(String[] args){
 		System.out.println("y");
 		new CodeOrder();
+		System.out.println("----");
+		new CodeOrder();
 		System.out.println("z");
 	}
 	//构造方法
--- src/main/java/StaticDemo/StaticTest.java
+++ src/main/java/StaticDemo/StaticTest.java
@@ -1,21 +1,20 @@
 package StaticDemo;
 public class StaticTest {
-    public static StaticTest t1 = new StaticTest();
-    public static StaticTest t2 = new StaticTest();
-
     {
         System.out.println("构造块");
     }
-
     static {
         System.out.println("静态块");
     }
+    public static StaticTest t1 = new StaticTest();
+    public static StaticTest t2 = new StaticTest();
 
     public StaticTest() {
         System.out.println("构造方法代码");
     }
 
     public static void main(String[] args) {
+    	System.out.println("main start");
         StaticTest t = new StaticTest();
     }
 }
